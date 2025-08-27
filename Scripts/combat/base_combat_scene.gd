fe591204ee920dbdd3db943a10c2c6187305fa31
jzzz <jianhengzhou003@gmail.com>
2025-08-27 16:31:35 -0700

    combat state machine work

--- Scripts/combat/base_combat_scene.gd
+++ Scripts/combat/base_combat_scene.gd
@@ -12,8 +12,24 @@ var state: CombatState = CombatState.SCHEDULER_IDLING
 var ally_combatants: Array[Combatant] = []
 var enemy_combatants: Array[Combatant] = []
 
+@onready var _scheduler: TurnScheduler = $"TurnScheduler"
+
 
 func init_combat():
+	for ally in ally_combatants:
+		_scheduler.attach_to_scheduler(ally)
+	for enemy in enemy_combatants:
+		_scheduler.attach_to_scheduler(enemy)
+	pass
+
+
+func state_update():
+	match(state):
+		CombatState.SCHEDULER_IDLING:
+			pass
+			give_turn(_scheduler.advance_scheduler())
+		CombatState.AWAITING_TURN_FINISH:
+			push_warning("Tried to update combat state machine while waiting.")
 	pass
 
 
@@ -25,4 +41,7 @@ func give_turn(combatant: Combatant):
 
 func _on_turn_finish(combatant: Combatant):
 	combatant.turn_finished.disconnect(_on_turn_finish)
+	combatant.readd_ended_turn()
+	_scheduler.sort_scheduler_queue()
+	state = CombatState.SCHEDULER_IDLING
 	pass
