bef41be171fc41f104ae689fe37e9d504a15cee5
jzzz <jianhengzhou003@gmail.com>
2025-08-29 13:03:17 -0700

    renamed a few things

--- Scripts/combat/combat_rules.gd
+++ Scripts/combat/combat_rules.gd
@@ -14,7 +14,11 @@ enum Elements {
 }
 
 
-func get_level_scaled_stat(base_stat: float, level: float, level_factor: float = DEFAULT_LEVEL_SCALE_FACTOR,
+func calc_level_scaled_stat(base_stat: float, level: float, level_factor: float = DEFAULT_LEVEL_SCALE_FACTOR,
 		mult_every_x: float = DEFAULT_MULT_EVERY_X) -> float:
 	var level_multiplier = pow(level_factor, level / mult_every_x)
 	return base_stat * level_multiplier
+
+
+func calculate_outgoing_damage():
+	pass
---
+++ Scripts/combat/stats/stat_spread_data.gd
@@ -0,0 +1,17 @@
+class_name StatSpreadData
+extends Resource
+
+@export_group("Base")
+@export var base_HP: float = 100
+@export var base_ATK: float = 10
+@export var base_DEF: float = 10
+
+@export_group("Scaling")
+@export var base_HP_scaling_factor: float = 2.5
+@export var base_ATK_scaling_factor: float = 2
+@export var base_DEF_scaling_factor: float = 2
+
+@export_group("Other")
+@export var base_resistances: ElementalSpreadData
+@export var added_crit_chance: float = 0
+@export var added_crit_multiplier: float = 0
