ff2106946dcdd904b3a4320b269e6b04823cb75c
jzzz <jianhengzhou003@gmail.com>
2025-08-29 14:59:12 -0700

    damage calcs

--- Scripts/combat/combat_rules.gd
+++ Scripts/combat/combat_rules.gd
@@ -20,5 +20,39 @@ func calc_level_scaled_stat(base_stat: float, level: float, level_factor: float 
 	return base_stat * level_multiplier
 
 
-func calculate_outgoing_damage():
-	pass
+func roll_for_crit(attacker: StatsComponent, attack: AttackData) -> bool:
+	var effective_crit_chance = attacker.additional_crit_chance + attack.critical_hit_chance
+	if randf() < effective_crit_chance:
+		return true
+	else:
+		return false
+
+
+func calc_damage(attacker: StatsComponent, defender: StatsComponent, 
+		attack: AttackData, is_crit: bool) -> float:
+	var outgoing_damage: float = _calculate_outgoing_damage(attack, attacker, is_crit)
+	var incoming_damage_modifier: float = _calculate_incoming_damage_modifier(defender, is_crit)
+	var elemental_modifier: float = _calculate_elemental_modifier(attack.element, defender, 
+		attacker.get_ele_pen(attack.element))
+	return outgoing_damage * incoming_damage_modifier * elemental_modifier
+
+
+func _calculate_outgoing_damage(attack: AttackData, attacker: StatsComponent, is_crit: bool) -> float:
+	if is_crit:
+		return (attacker.effective_ATK * attack.power * attacker.effective_outdmg_multiplier * 
+			(attack.crit_multiplier + attacker.additional_crit_multiplier))
+	else:
+		return attacker.effective_ATK * attack.power * attacker.effective_outdmg_multiplier
+
+
+func _calculate_incoming_damage_modifier(defender: StatsComponent, is_crit: bool) -> float:
+	if is_crit:
+		return (1 / defender.effective_DEF_for_crit) * defender.effective_indmg_reduction
+	else:
+		return (1 / defender.effective_DEF) * defender.effective_indmg_reduction
+		
+
+
+func _calculate_elemental_modifier(element: CombatRules.Elements, defender: StatsComponent,
+		penetration: float) -> float:
+	return defender.get_resist_multiplier(element) + penetration
