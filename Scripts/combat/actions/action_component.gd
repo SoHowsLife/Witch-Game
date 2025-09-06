3cf1cb5080120da63cb9a2e2c80665e985cb7753
jzzz <jianhengzhou003@gmail.com>
2025-09-05 17:19:36 -0700

    ga

--- Scripts/combat/actions/action_component.gd
+++ Scripts/combat/actions/action_component.gd
@@ -3,7 +3,13 @@ extends Node
 
 var possible_attacks: Array[ActionData]
 var possible_supports: Array[ActionData]
-var possible_else: Array[ActionData]
+var possible_other: Array[ActionData]
+
+
+func init_actions(action_spread: ActionSpreadData):
+	possible_attacks = action_spread.possible_attacks.duplicate()
+	possible_supports = action_spread.possible_support.duplicate()
+	possible_other = action_spread.possible_other.duplicate()
 
 
 func request_agent_input():
